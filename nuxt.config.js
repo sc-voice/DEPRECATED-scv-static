b40fca4338c22191e451c072d7fdf446910e0ce0
Karl Lew <karl@firepick.org>
2020-12-17 12:19:24 -0800

    referrer

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'referrer', content:'strict-origin-when-cross-origin' },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
