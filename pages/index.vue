5b11281f99353f6ede6815224d170552cc448b89
Karl Lew <karl@firepick.org>
2021-03-08 10:42:15 -0800

    v0.4.32 scv-page-suttas

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
-    <div class="text-h5"> Choose view </div>
-    <ul>
-        <li><nuxt-link to="/main">Main</nuxt-link></li>
-        <li><nuxt-link to="/components">Components</nuxt-link></li>
-        <li><nuxt-link to="/news/2021-02-25">News</nuxt-link></li>
-        <li><nuxt-link to="/wiki/">Wiki</nuxt-link></li>
-    </ul>
-  </div>
+  <v-card flat>
+    <v-card-text class="nuxt-content">
+      <h1> Welcome to Scv-Static</h1>
+      <ul>
+          <li><nuxt-link to="/suttas">Suttas</nuxt-link></li>
+          <li><nuxt-link to="/news/2021-02-25">News</nuxt-link></li>
+          <li><nuxt-link to="/wiki/">Wiki</nuxt-link></li>
+          <li><nuxt-link to="/components">Components</nuxt-link></li>
+      </ul>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
--- pages/suttas.vue
+++ pages/suttas.vue
@@ -1,87 +1,19 @@
-
 <template>
-  <v-sheet light class="desktop" >
-    <div class="desktop-search">
-      <scv-search-field :js="js"
-        v-on:search-text="search"
-      />
-      <scv-results :js="js"/>
-    </div>
-    <scv-sutta :js="js" />
-  </v-sheet>
+  <scv-page-suttas>
+  </scv-page-suttas>
 </template>
 
 <script>
-import Vue from 'vue';
 import { ScvVue } from '../index';
 let {
-  ScvSearchField,
-  ScvResults,
-  ScvSettings,
-  ScvSutta,
+  ScvPageSuttas,
 } = ScvVue;
-
-const JS = {
-  BilaraWeb: require('../src/bilara-web'),
-  Tipitaka: require('scv-bilara/src/tipitaka'),
-}
 
 export default {
   components: {
-    ScvSearchField,
-    ScvResults,
-    ScvSutta,
-    ScvSettings,
-  },
-  data: function(){
-    return {
-      panels: [0,1],
-      monolingual: null,
-    };
-  },
-  mounted() {
-  },
-  methods:{
-    search(value) {
-      console.log(`search:`, value);
-    },
-    githubUrl(path) {
-      return `https://github.com/sc-voice/scv-static/blob/main/${path}`;
-    },
-  },
-  computed: {
-    version() {
-      return require('../package.json').version;
-    },
-    js() { 
-      return JS;
-    },
+    ScvPageSuttas,
   },
 }
 </script>
 <style>
-.v-application code {
-  background-color: #000;
-}
-
-.desktop-search {
-  xmax-width: 28rem;
-}
-
-.desktop-nav {
-  width: 100%;
-  padding: 0.5rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  display: flex;
-  justify-content: space-between;
-  background-color: #eee;
-}
-
-.desktop {
-  display: flex;
-  flex-flow: column;
-  align-items: center;
-}
-
 </style>
