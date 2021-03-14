ceffe901272d3a6dc440d7413cb99b3eec4c2915
Karl Lew <karl@firepick.org>
2021-03-14 05:45:47 -0700

    v0.4.67 redirect

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,8 @@
 <template>
-  <v-card flat>
-    <v-card-text class="nuxt-content">
-      <h1> Welcome to Scv-Static</h1>
-      <ul>
-          <li><nuxt-link to="/suttas">Suttas</nuxt-link></li>
-          <li><nuxt-link to="/news/2021-02-25">News</nuxt-link></li>
-          <li><nuxt-link to="/wiki/">Wiki</nuxt-link></li>
-          <li><nuxt-link to="/components">Components</nuxt-link></li>
-      </ul>
-    </v-card-text>
-  </v-card>
+  <div>redirecting...</div>
 </template>
-
 <script>
-
 export default {
-  components: {
-  },
-  data: function(){
-    return {
-    };
-  },
-  mounted() {
-  },
-  methods:{
-  },
-  computed: {
-  },
+  middleware: 'home-redirect',
 }
 </script>
-<style>
-</style>
